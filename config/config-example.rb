01847c6d50ab1898d22d689e935aef785f38fb2a
Eaden McKee <eadz@eadz.co.nz>
2010-02-16 15:20:24 +1300

    updated readme and example config

--- config/config-example.rb
+++ config/config-example.rb
@@ -2,4 +2,10 @@ AMAZON_ACCESS_KEY_ID='AAAAAAAAA'
 AMAZON_SECRET_ACCESS_KEY='AAAAAAA'
 BUCKET_NAME = 'backupbucket'
 
+# directories to archive
+# you can use any method you want to get these directories here rather than hand coding them
 @dirs << {:name => 'gitosis', :dir => '/srv/gitosis'}
+
+%w{bob joe dave}.each do |user|
+  @dirs << {:name => "backup-#{user}", :dir => "/home/#{user}"}
+end
